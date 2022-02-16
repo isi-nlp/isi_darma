7ab62ce6b4a0ea80cda73cd7e619fffaa72ba5ee
Darpan Jain <darpan.pmun@gmail.com>
2022-02-16 09:35:32 -0800

    Minor formatting

--- src/isi_darma/pipeline/moderation_classifiers.py
+++ src/isi_darma/pipeline/moderation_classifiers.py
@@ -3,43 +3,45 @@ from abc import ABC, abstractmethod
 from isi_darma.logging_setup import logger
 import json
 
-
 API_KEY = 'AIzaSyC30WbnABE2zjzK4Be58ytkatxgOC3yg9I'
+
 
 class ModerationClassifier(ABC):
 
-    @abstractmethod
-    def needs_moderation(self, toxicity):
-        pass
-class PerspectiveAPIModerator(ModerationClassifier): 
+	@abstractmethod
+	def needs_moderation(self, toxicity):
+		pass
 
-    def __init__(self) -> None:
-        
-        self.client = discovery.build(
-        "commentanalyzer",
-        "v1alpha1",
-        developerKey=API_KEY,
-        discoveryServiceUrl="https://commentanalyzer.googleapis.com/$discovery/rest?version=v1alpha1",
-        static_discovery=False,
-        )
 
-        self.toxicity_threshold = 0.7
+class PerspectiveAPIModerator(ModerationClassifier):
 
-    def needs_moderation(self, toxicity) -> bool: 
-        return toxicity >= self.toxicity_threshold
+	def __init__(self) -> None:
 
-    def measure_toxicity(self, comment) -> float:
+		self.client = discovery.build(
+			"commentanalyzer",
+			"v1alpha1",
+			developerKey=API_KEY,
+			discoveryServiceUrl="https://commentanalyzer.googleapis.com/$discovery/rest?version=v1alpha1",
+			static_discovery=False,
+		)
 
-        analyze_request = {
-            'comment': { 'text': comment },
-            'requestedAttributes': {'TOXICITY': {}}
-        }
+		self.toxicity_threshold = 0.7
 
-        response = self.client.comments().analyze(body=analyze_request).execute()
-        try: 
-            toxicity_score = float(response["attributeScores"]["TOXICITY"]["summaryScore"]["value"])
-        except Exception as e: 
-            logger.info(f"Exception occurred: {e}. Setting toxicity to 0.")
-            toxicity_score = 0 
-    
-        return toxicity_score
+	def needs_moderation(self, toxicity) -> bool:
+		return toxicity >= self.toxicity_threshold
+
+	def measure_toxicity(self, comment) -> float:
+
+		analyze_request = {
+			'comment': {'text': comment},
+			'requestedAttributes': {'TOXICITY': {}}
+		}
+
+		response = self.client.comments().analyze(body=analyze_request).execute()
+		try:
+			toxicity_score = float(response["attributeScores"]["TOXICITY"]["summaryScore"]["value"])
+		except Exception as e:
+			logger.info(f"Exception occurred: {e}. Setting toxicity to 0.")
+			toxicity_score = 0
+
+		return toxicity_score
