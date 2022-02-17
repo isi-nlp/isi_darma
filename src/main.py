9b811135cdb66ef96196df2422e34d9f08f7d9fd
Darpan Jain <darpan.pmun@gmail.com>
2022-02-16 18:00:46 -0800

    Minor formatting changes

--- src/main.py
+++ src/main.py
@@ -7,24 +7,24 @@ from argparse import ArgumentParser
 logger.info("\n\n\n")
 SUBREDDIT = "darma_test"
 
+
 def main():
+	parser = ArgumentParser()
+	parser.add_argument("--test", "-t", action="store_true")
+	args = parser.parse_args()
 
-    parser = ArgumentParser()
-    parser.add_argument("--test", "-t", action="store_true")
-    args = parser.parse_args()
+	reddit_client = load_reddit_client()
+	logger.info("Instantiated Reddit Client")
 
-    reddit_client = load_reddit_client()
-    logger.info("Instantiated Reddit Client")
+	moderation_bot = BasicBot(test=args.test)
 
-    moderation_bot = BasicBot(test = args.test)
-    
-    subreddit = reddit_client.subreddit(SUBREDDIT)
+	subreddit = reddit_client.subreddit(SUBREDDIT)
 
-    for submission in subreddit.stream.submissions():
-        moderation_bot.moderate_submission(submission)
+	for submission in subreddit.stream.submissions():
+		moderation_bot.moderate_submission(submission)
 
-    return
+	return
 
 
 if __name__ == "__main__":
-    main()
+	main()
