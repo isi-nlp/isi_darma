c4666e0f33a000c301af6505de22c5c7036a73bb
Darpan Jain <darpan.pmun@gmail.com>
2022-02-16 18:00:31 -0800

    Added StreamHandler for logging during tests

--- src/isi_darma/logging_setup.py
+++ src/isi_darma/logging_setup.py
@@ -3,15 +3,15 @@ import logging
 from logging.handlers import RotatingFileHandler
 
 
-def setup_logger(logname, filename):
+def setup_logger(logname, filename, test=False):
     """
     Creates a logging rotating file handlers for writing and
     size-based log file rotation
 
     """
 
-    if not os.path.exists('../logs'):
-        os.mkdir('../logs')
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
 
     log_format = '<%(asctime)s> [%(levelname)s] <%(lineno)s> <%(funcName)s> %(message)s'
     file_logger = RotatingFileHandler(filename, mode='a', maxBytes=500 * 1024 * 1024, backupCount=10)
@@ -22,7 +22,11 @@ def setup_logger(logname, filename):
     logger = logging.getLogger(logname)
     logger.setLevel(logging.DEBUG)
     logger.addHandler(file_logger)
+
+    if test:
+        consoleHandler = logging.StreamHandler()
+        consoleHandler.setFormatter(formatter)
+        consoleHandler.setLevel(logging.DEBUG)
+        logger.addHandler(consoleHandler)
+
     return logger
-
-
-logger = setup_logger('app', '../logs/app.log')
