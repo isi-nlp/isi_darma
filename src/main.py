ece1caea5457e1c0075a38f0017dfe6fcf23463e
Darpan Jain <darpan.pmun@gmail.com>
2022-03-07 22:59:24 -0800

    Polling for replying to new comments and posts

--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
 from isi_darma.utils import load_reddit_client
 from isi_darma.bots import BasicBot
+from time import sleep
 
 from argparse import ArgumentParser
 
 SUBREDDIT = "darma_test"
 
 
-def main():
+def main(test=False):
 	parser = ArgumentParser()
 	parser.add_argument("--test", "-t", action="store_true")
 	args = parser.parse_args()
@@ -16,13 +17,14 @@ def main():
 	reddit_client = load_reddit_client(moderation_bot.logger)
 	moderation_bot.logger.info("Instantiated Reddit Client")
 
-	subreddit = reddit_client.subreddit(SUBREDDIT)
+	while True:
 
-	# TODO: Poll for new replies and posts
-	for submission in subreddit.stream.submissions():
-		moderation_bot.moderate_submission(submission)
+		subreddit = reddit_client.subreddit(SUBREDDIT)
+		for submission in subreddit.stream.submissions():
+			moderation_bot.moderate_submission(submission)
 
-	return
+		# moderation_bot.logger.info(f'------------ NO MORE POSTS/COMMENTS TO RESPOND TO! SLEEPING NOW... ------------')
+		# sleep(15*60)
 
 
 if __name__ == "__main__":
