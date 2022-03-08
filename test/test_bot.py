97d3dd7b2712886413b49f341b26463091aa53dc
Darpan Jain <darpan.pmun@gmail.com>
2022-03-07 21:54:57 -0800

    Added more toxic dialogues for testing

--- test/test_bot.py
+++ test/test_bot.py
@@ -1,6 +1,7 @@
 from isi_darma.bots import BasicBot
 
 bot = BasicBot(test=True)
-dialogue = ["Hello", "I hate you"]
+dialogue = ["Hello", "I hate you", "You're such a jerk", "I will beat you up!"]
 
-print(bot.moderate(dialogue_str=dialogue))
+for diag in dialogue:
+	print(f'Final Response to {diag} from the bot --> ', bot.moderate(dialogue_str=dialogue))
