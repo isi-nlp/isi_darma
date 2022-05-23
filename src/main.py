532cb645f85be5a9f595a2e6f6df319c4f4d115a
Darpan Jain <darpan.pmun@gmail.com>
2022-05-23 14:05:38 -0700

    Streaming for posts and comments

--- src/main.py
+++ src/main.py
@@ -17,12 +17,28 @@ def main():
     reddit_client = load_reddit_client(moderation_bot.logger)
     moderation_bot.logger.info("Instantiated Reddit Client")
 
-	# TODO: Stream doesn't track new comments on existing posts and threads
-	subreddit = reddit_client.subreddit(SUBREDDIT)
-	for submission in subreddit.stream.submissions():
-		moderation_bot.moderate_submission(submission)
+    subreddit = reddit_client.subreddit(SUBREDDIT)
+    posts = subreddit.stream.submissions(pause_after=-1)
+    cmts = subreddit.stream.comments(pause_after=-1, skip_existing=True)
+    # stream = praw.models.util.stream_generator(stream_all(subreddit))
 
-	return
+    while True:
+        for post in posts:
+            if post is None:
+                break
+            # print("POST: ", post.title)
+            moderation_bot.moderate_submission(post)
+
+        for cmt in cmts:
+            if cmt is None:
+                break
+            # print("CMT: ", cmt.body[:50])
+            moderation_bot.moderate_comment_thread(cmt)
+            # moderation_bot.moderate_submission(cmt, title=cmt.submission.title, post_body=cmt.submission.selftext)
+
+    # for idx, submission in enumerate(stream):
+    # 	print(idx+1, submission.body)
+        # moderation_bot.moderate_submission(submission)
 
 
 if __name__ == "__main__":
