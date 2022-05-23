141ad5e7b46a54bee4a06d8da3fa586ce3ea120c
Darpan Jain <darpan.pmun@gmail.com>
2022-05-23 14:00:12 -0700

    Minor formatting

--- src/isi_darma/utils.py
+++ src/isi_darma/utils.py
@@ -10,30 +10,30 @@ CRED_FN = os.environ.get("CRED_FP", "/isi_darma/isi_darma/src/isi_darma/creds.ya
 
 
 def get_username(redditor_obj: Redditor):
-    """
+	"""
     Get the username of a redditor
     Redditor object reference: https://praw.readthedocs.io/en/latest/code_overview/models/redditor.html#praw.models.Redditor
     """
-    return redditor_obj.author
+	return redditor_obj.author
 
 
 def load_credentials(logger: Logger, creds_fn: str = CRED_FN) -> Dict[str, str]:
-    with open(creds_fn, "r") as f:
-        creds = yaml.safe_load(f)
+	with open(creds_fn, "r") as f:
+		creds = yaml.safe_load(f)
 
-    logger.info(f"Loaded credentials: {creds}")
-    return creds
+	logger.info(f"Loaded credentials: {creds}")
+	return creds
 
 
 def load_reddit_client(logger):
-    creds = load_credentials(logger)
+	creds = load_credentials(logger)
 
-    reddit = praw.Reddit(
-        user_agent=f"reddit:darma:0 (by u/{creds['username']})",
-        client_id=creds["client_id"],
-        client_secret=creds["client_secret"],
-        username=creds["username"],
-        password=creds["password"]
-    )
+	reddit = praw.Reddit(
+		user_agent=f"reddit:darma:0 (by u/{creds['username']})",
+		client_id=creds["client_id"],
+		client_secret=creds["client_secret"],
+		username=creds["username"],
+		password=creds["password"]
+	)
 
-    return reddit
+	return reddit
