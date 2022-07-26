054f33f35d1e5ca54c0d22fbd7fd3ec9b07572eb
Darpan Jain <darpan.pmun@gmail.com>
2022-07-26 01:16:12 -0700

    Replace English template responses with French translations

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,12 @@
+
+U
+%src/isi_darma/pipeline/translators.py,8/a/8a4b67dac9553467f4430fa349da9c0ccb4a69f8
+@
+requirements.txt,1/9/19359a61ae2446b51b549167b014da2fcf265768
+:
+
+Dockerfile,6/6/6651ddff6eb82c840ced7c1dddee15c6e1913dd4
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+8
+build.sh,5/9/59f81c123b3abbcb97274545796dd18706c3e106
--- src/isi_darma/pipeline/response_generators.py
+++ src/isi_darma/pipeline/response_generators.py
@@ -45,12 +45,11 @@ class SpolinBotRG(ResponseGenerator):
 	@staticmethod
 	def get_random_comtype_resp(usernames=['others']):
 		usernames = ",".join(usernames)
-		# TODO: replace with french translations of the responses
 		comtype_responses = [
-			f'Please quit doing that and behave!',
-			f'You used this kind of behavior in response to {usernames}.',
-			f'You used this kind of behavior in response to {usernames}. I feel upset because even though I don’t know {usernames}, the language you used when communicating with them would make me upset.',
-			f'You used this kind of behavior in response to {usernames}. Is this because you are angry at {usernames} for having beliefs that are different from yours?',
-			f'You used this kind of behavior in response to {usernames}. Is this because you are angry at {usernames} for having beliefs that are  different from yours? Do you want {usernames} to change their opinions and perhaps convince others who currently share their opinions to also change?'
+			f"Veuillez cesser de faire cela et vous comporter correctement!",
+			f"Vous avez eu un comportement de ce genre en répondant à {usernames}",
+			f"Je me sens contrarié parce que même si je ne connais pas {usernames}, le langage que vous avez utilisé pour communiquer avec eux me bouleverserait.",
+			f"Est-ce parce que vous êtes en colère contre {usernames} parce qu'ils ont des croyances différentes des vôtres?",
+			f"Voulez-vous que {usernames} changent d'opinion et peut-être convaincre d'autres personnes qui partagent actuellement leurs opinions de changer également?"
 		]
 		return random.sample(comtype_responses, 1)[0]
