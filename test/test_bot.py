13f5c1d7723559940949136f8d7142fc04f0fc19
Darpan Jain <darpan.pmun@gmail.com>
2022-08-07 13:36:17 -0700

    Remove path to db being passed to `BasicBot` instance from `test_bot.py`

--- test/test_bot.py
+++ test/test_bot.py
@@ -1,6 +1,6 @@
 from isi_darma.bots import BasicBot
 
-bot = BasicBot(test=True, db = "/Users/darpanjain/Data/USC/RA - ISI/isi_darma/src/isi_darma/data/optout/optout_db.json")
+bot = BasicBot(test=True)
 dialogue = ["Hello", "I hate you", "You're such a jerk", "I will beat you up!", "Hey asshole!", "opt-out please"]
 
 for diag in dialogue:
