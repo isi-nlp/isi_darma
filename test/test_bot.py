8c10360250aa868fa187d5fd39c4882eb60cfbe1
Darpan Jain <darpan.pmun@gmail.com>
2022-08-07 14:22:01 -0700

    Print statements enhanced during tests

--- test/test_bot.py
+++ test/test_bot.py
@@ -4,4 +4,5 @@ bot = BasicBot(test=True)
 dialogue = ["Hello", "I hate you", "You're such a jerk", "I will beat you up!", "Hey asshole!", "opt-out please"]
 
 for diag in dialogue:
-	print(f'*** Final Response to "{diag}" from the bot --> {bot.moderate(dialogue_str=diag)}\n')
+	final_response = bot.moderate(dialogue_str=diag)
+	print(f'*** Final Response to "{diag}" from the bot -->\n{final_response if final_response else " -> No Response <-"}\n')
