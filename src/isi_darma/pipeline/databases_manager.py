139e6ff6a809b76521d462041b39260857f4a64e
Darpan Jain <darpan.pmun@gmail.com>
2022-08-07 14:59:22 -0700

    Formatting and logging added

--- src/isi_darma/pipeline/databases_manager.py
+++ src/isi_darma/pipeline/databases_manager.py
@@ -2,6 +2,7 @@ import json
 
 class DatabaseManager:
 
+
     def __init__(self, logger, root = "isi_darma/data"):
         self.logger = logger
         self.root_dir = root
@@ -10,16 +11,19 @@ class DatabaseManager:
         self.optout_db = self.read_db(self.optout_db_path)
         self.moderated_db = self.read_db(self.moderated_hashes_path)
 
-    @staticmethod
-    def read_db(path):
+
+    def read_db(self, path):
+        self.logger.info(f"Reading database from {path}")
         with open(path, 'r') as f:
             return json.load(f)
+
 
     def add_optout_user(self, username: str, dialogue_str: str):
         self.optout_db[username] = { "dialogue": dialogue_str }
         with open(self.optout_db_path, "w") as f:
             f.write(json.dumps(self.optout_db))
         self.logger.info(f"User {username} opted out of darma bot moderation.")
+
 
     def search_optout_db(self, username: str) -> bool:
         user_in_optout = username in self.optout_db.keys()
