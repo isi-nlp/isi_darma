26999ee1720bb3e81cd119257dcd70a757f0520b
Thamme Gowda <tgowdan@gmail.com>
2022-08-08 09:55:05 -0700

    simplify TurkLikeGptAgent by reusing code

--- darma_chat/darma_chat/gpt_agent.py
+++ darma_chat/darma_chat/gpt_agent.py
@@ -1,36 +1,16 @@
-from parlai.core.params import ParlaiParser
-import openai  
-
-from typing import Dict
-
-from omegaconf import DictConfig
-import parlai.utils.logging as logging
-from parlai.core.agents import create_agent
-from parlai.core.params import ParlaiParser
-from darma_chat.constants import AGENT_1
+from darma_chat.bot_agent import TurkLikeAgent, logging
+import openai
 
 
-class TurkLikeGptAgent:
+
+class TurkLikeGptAgent(TurkLikeAgent):
     """
     Will act like a Turker but actually contains a bot agent.
     """
 
-    def __init__(self, opt, model_name, num_turns, semaphore=None):
-        self.opt = opt
-        self.id = AGENT_1
-        self.num_turns = num_turns
-        self.turn_idx = 0
-        self.semaphore = semaphore
-        self.worker_id = model_name
-        self.hit_id = 'none'
-        self.assignment_id = 'none'
-        self.some_agent_disconnected = False
-        self.hit_is_abandoned = False
-        self.hit_is_returned = False
-        self.disconnected = False
-        self.hit_is_expired = False
-        self.sturns = ''
-        openai.api_key = '' 
+    def __init__(self, *args, api_key='', **kwargs):
+        super().__init__(*args, **kwargs)
+        openai.api_key = api_key
 
     def act(self, timeout=None):
         instr = "Respond to the last user using nonviolent communication"
@@ -56,53 +36,6 @@ class TurkLikeGptAgent:
     def observe(self, observation, increment_turn: bool = True):
         self.sturns += f"user {observation['id']}: {observation['text']}\n"
 
-    def shutdown(self):
-        pass
-
-    def reset(self):
-        self.model_agent.reset()
-
-    @staticmethod
-    def get_bot_agents(
-        args: DictConfig, model_opts: Dict[str, str], no_cuda=False
-    ) -> Dict[str, dict]:
-        """
-        Return shared bot agents.
-
-        Pass in model opts with the `model_opts` arg, where `model_opts` is a dictionary
-        whose keys are model names and whose values are strings that specify model
-        params (i.e. `--model image_seq2seq`).
-        """
-
-        # Set up overrides
-        model_overrides = {'model_parallel': args.blueprint.task_model_parallel}
-        if no_cuda:
-            # If we load many models at once, we have to keep it on CPU
-            model_overrides['no_cuda'] = no_cuda
-        else:
-            logging.warning(
-                'WARNING: MTurk task has no_cuda FALSE. Models will run on GPU. Will '
-                'not work if loading many models at once.'
-            )
-
-        # Convert opt strings to Opt objects
-        processed_opts = {}
-        for name, opt_string in model_opts.items():
-            parser = ParlaiParser(True, True)
-            parser.set_params(**model_overrides)
-            processed_opts[name] = parser.parse_args(opt_string.split())
-
-        # Load and share all model agents
-        logging.info(
-            f'Got {len(list(processed_opts.keys()))} models: {processed_opts.keys()}.'
-        )
-        shared_bot_agents = {}
-        for model_name, model_opt in processed_opts.items():
-            logging.info('\n\n--------------------------------')
-            logging.info(f'model_name: {model_name}, opt_dict: {model_opt}')
-            model_agent = create_agent(model_opt, requireModelExists=True)
-            shared_bot_agents[model_name] = model_agent.share()
-        return shared_bot_agents
 
 def query_completion_api(prompt, engine='text-davinci-002', frequency_penalty=0, presence_penalty=0):
     response = openai.Completion.create(
@@ -114,7 +47,6 @@ def query_completion_api(prompt, engine='text-davinci-002', frequency_penalty=0,
         frequency_penalty=frequency_penalty,
         presence_penalty=presence_penalty
     )
-    
     return response
 
 def prompt_compose(instr, few_shot_example, seed_turns):
