17ebf48cae45ab586010f644abb6dbb5e79c9c74
Thamme Gowda <tgowdan@gmail.com>
2022-08-26 22:49:38 -0700

    chat admin: approve and qualify workers

--- chat_admin/chat_admin/app.py
+++ chat_admin/chat_admin/app.py
@@ -19,6 +19,8 @@ import flask
 from flask import Flask, request, send_from_directory, Blueprint
 import boto3
 from ruamel.yaml import YAML
+from cachetools import cached, LRUCache, TTLCache
+
 
 from .utils import max_RSS, format_bytes
 
@@ -34,6 +36,9 @@ bp = Blueprint('admin', __name__, template_folder='templates', static_folder='st
 
 
 MTURK_SANDBOX = 'https://mturk-requester-sandbox.us-east-1.amazonaws.com'
+AWS_CACHE_TTL = 60 * 1 # seconds
+AWS_CACHE_MAXSIZE = 64
+AWS_MAX_RESULTS = 100
 
 
 sys_info = {
@@ -125,9 +130,10 @@ class DashboardService:
     def get_chat(self, chat_id):
         chat_file = self.chat_files[chat_id]
         with open(chat_file, encoding='utf8', errors='replace') as fobj:
-            return json.load(fobj)
+            chat = json.load(fobj)
+        return chat
 
-    def list_qualification_types(self, max_results=100, query: str=''):
+    def list_qualification_types(self, max_results=AWS_MAX_RESULTS, query: str=''):
         data = self.mturk.list_qualification_types(
             MustBeRequestable=True, MustBeOwnedByCaller=True,
             MaxResults=max_results)
@@ -138,12 +144,28 @@ class DashboardService:
                     if query in qt['Name'].lower() or query in qt['Description']]
         return qtypes
 
-    def list_HITS(self, qual_id:str, max_results=100):
+    @cached(cache=TTLCache(maxsize=AWS_CACHE_MAXSIZE, ttl=AWS_CACHE_TTL))
+    def list_HITS(self, qual_id:str, max_results=AWS_MAX_RESULTS):
         return self.mturk.list_hits_for_qualification_type(
             QualificationTypeId=qual_id,
             MaxResults=max_results)
 
-    def list_assignments(self, HIT_id: str, max_results=100):
+    @cached(cache=TTLCache(maxsize=AWS_CACHE_MAXSIZE, ttl=AWS_CACHE_TTL))
+    def list_workers_for_qualtype(self, qual_id:str, max_results=AWS_MAX_RESULTS):
+        return self.mturk.list_workers_with_qualification_type(
+            QualificationTypeId=qual_id,
+            MaxResults=max_results)
+
+    @cached(cache=TTLCache(maxsize=AWS_CACHE_MAXSIZE, ttl=AWS_CACHE_TTL))
+    def list_all_hits(self, max_results=AWS_MAX_RESULTS, next_token=None):
+        args = dict(MaxResults=max_results)
+        if next_token:
+            args['NextToken'] = next_token
+        return self.mturk.list_hits(**args)
+
+
+    @cached(cache=TTLCache(maxsize=AWS_CACHE_MAXSIZE, ttl=AWS_CACHE_TTL))
+    def list_assignments(self, HIT_id: str, max_results=AWS_MAX_RESULTS):
         return self.mturk.list_assignments_for_hit(
             HITId=HIT_id, MaxResults=max_results)
 
@@ -160,15 +182,15 @@ class DashboardService:
             QualificationTypeId=qual_id,
             WorkerId=worker_id,
             IntegerValue=1,
-            SendNotification=True
+            SendNotification=send_email
             )
 
     def disqualify_worker(self, worker_id: str, qual_id: str, reason: str=None):
         log.info(f"Disqualifying worker: {worker_id} for {qual_id}")
-        return self.mturk.disassociate_qualification_with_worker(
+        return self.mturk.disassociate_qualification_from_worker(
             QualificationTypeId=qual_id,
             WorkerId=worker_id,
-            Reason=None
+            Reason=reason
             )
 
 ######### END Services ##############
@@ -200,38 +222,80 @@ def attach_admin_dashboard(config):
     def review_chat(chat_id):
         if request.method != 'GET':
             return "Only GET method supported", 400
-        if not chat_id:
-            return "chat_id required", 400
         if chat_id not in dboard.chat_files:
             return f"chat ID {chat_id} unknown", 400
         chat_data = dboard.get_chat(chat_id)
-        return render_template('chatui.html', data=chat_data, chat_id=chat_id)
+        mturk_asgn_id = chat_data.get('mturk', {}).get('assignment_id')
+        qtypes = None
+        if mturk_asgn_id:
+            # dont cache assignment
+            assignment = dboard.mturk.get_assignment(AssignmentId=mturk_asgn_id)['Assignment']
+            chat_data['mturk'].update(assignment)
+            qtypes = dboard.list_qualification_types(max_results=AWS_MAX_RESULTS)
+        return render_template('chatui.html', data=chat_data, chat_id=chat_id, qtypes=qtypes)
 
-    @bp.route("/mturk/qualification", methods=["GET"])
+    @bp.route("/mturk/", methods=["GET"])
+    def mturk_home():
+        meta = dict(endpoint_url = dboard.mturk.meta.endpoint_url)
+        return render_template('mturk_home.html',meta=meta)
+
+    @bp.route("/mturk/qualification/", methods=["GET"])
     def mturk_qualifications():
-        qtypes = dboard.list_qualification_types(max_results=100)
+        qtypes = dboard.list_qualification_types(max_results=AWS_MAX_RESULTS)
         meta = dict(endpoint_url = dboard.mturk.meta.endpoint_url)
         return render_template('mturk_qualifications.html', qtypes=qtypes, meta=meta)
 
     @bp.route("/mturk/qualification/<qual_id>", methods=["GET"])
     def mturk_qualification(qual_id):
-        data = dboard.list_HITS(qual_id=qual_id,max_results=100)
-        return render_template('mturk_HITs.html', data=data, qual_id=qual_id)
+        HITs = dboard.list_HITS(qual_id=qual_id,max_results=AWS_MAX_RESULTS)
+        workers = dboard.list_workers_for_qualtype(qual_id=qual_id, max_results=AWS_MAX_RESULTS)
+        data = dict(HITs=HITs['HITs'], workers=workers['Qualifications'])
+        return render_template('mturk_qualification.html', data=data, qual_id=qual_id)
 
     @bp.route("/mturk/qualification/<qual_id>", methods=["DELETE"])
     def delete_qualification(qual_id):
         data = dboard.mturk.delete_qualification_type(QualificationTypeId=qual_id)
         return jsonify(data), 200
 
+
+    @bp.route("/mturk/HIT/", methods=["GET"])
+    def mturk_HITs():
+        data = dboard.list_all_hits()
+        return render_template('mturk_HITs.html', data=data)
+
     @bp.route("/mturk/HIT/<HIT_id>", methods=["GET"])
     def mturk_assignments(HIT_id):
-        data = dboard.list_assignments(HIT_id=HIT_id,max_results=100)
-        return jsonify(data), 200
+        data = dboard.list_assignments(HIT_id=HIT_id, max_results=100)
+        qtypes = dboard.list_qualification_types(max_results=100)
+        return render_template('mturk_HIT.html', data=data, HIT_id=HIT_id, qtypes=qtypes)
 
     @bp.route("/mturk/HIT/<HIT_id>", methods=["DELETE"])
     def delete_hit(HIT_id):
         data = dboard.mturk.delete_hit(HITId=HIT_id)
-        return jsonify(data), 200
+        return jsonify(data), data.get('HTTPStatusCode', 200)
+
+    @bp.route("/mturk/assignment/<asgn_id>/approve", methods=["POST"])
+    def approve_assignment(asgn_id):
+        #RequesterFeedback=feedback # any feed back message to worker
+        data = dboard.mturk.approve_assignment(AssignmentId=asgn_id)
+        return jsonify(data), data.get('HTTPStatusCode', 200)
+
+    @bp.route("/mturk/worker/<worker_id>/qualification", methods=["POST", "PUT"])
+    def qualify_worker(worker_id):
+        qual_id = request.form.get('QualificationTypeId')
+        log.info(f"Qualify: worker: {worker_id}  to qualification: {qual_id}")
+        if not qual_id:
+            return 'ERROR: QualificationTypeId argument is requires', 400
+        data = dboard.qualify_worker(worker_id=worker_id, qual_id=qual_id)
+        return jsonify(data), data.get('HTTPStatusCode', 200)
+
+    @bp.route("/mturk/worker/<worker_id>/qualification/<qual_id>", methods=["DELETE"])
+    def disqualify_worker(worker_id, qual_id):
+        log.info(f"Disqualify: worker: {worker_id} from qualification: {qual_id}")
+        reason = request.values.get('reason', '')
+        data = dboard.disqualify_worker(worker_id=worker_id,qual_id=qual_id, reason=reason)
+        return jsonify(data), data.get('HTTPStatusCode', 200)
+
 
     @bp.route('/about')
     def about():
