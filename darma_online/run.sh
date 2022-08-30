53e0e3e5b029cbff9ea32cdfb7daef713f5e8cf1
Darpan Jain <darpan.pmun@gmail.com>
2022-08-29 21:03:34 -0700

    Minor fix in volume mount

--- darma_online/run.sh
+++ darma_online/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run -i --rm -dP -v /isi_darma/docker_logs/:/logs/ -v /isi_darma/isi_darma/darma_online/src/darma_online/data:/isi_darma/isi_darma/darma_online/src/darma_online/data/conversations --name $1 isi_darma:latest && docker logs -f $1
+docker run -i --rm -dP -v /isi_darma/docker_logs/:/logs/ -v /isi_darma/isi_darma/darma_online/src/darma_online/data/conversations/:/isi_darma/isi_darma/darma_online/src/darma_online/data/conversations --name $1 isi_darma:latest && docker logs -f $1
