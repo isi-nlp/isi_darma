e08099587f53b81aa24cb874ae282ffb9907949c
Darpan Jain <darpan.pmun@gmail.com>
2022-08-29 20:30:09 -0700

    Minor typo fix

--- darma_online/run.sh
+++ darma_online/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run -i --rm -dP -v /isi_darma/docker_logs/: /isi_darma/isi_darma/darma_online/src/logs/ --name $1 isi_darma:latest && docker logs -f $1
+docker run -i --rm -dP -v /isi_darma/docker_logs/:/isi_darma/isi_darma/darma_online/src/logs/ --name $1 isi_darma:latest && docker logs -f $1
