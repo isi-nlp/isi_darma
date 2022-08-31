8a9cfc0ccc68af3404cfd85d46071f3890ffd1fd
Thamme Gowda <tgowdan@gmail.com>
2022-08-30 19:19:49 -0700

    HIT: enable locales and max_assignments=9

--- darma_chat/darma_chat/run.py
+++ darma_chat/darma_chat/run.py
@@ -4,15 +4,17 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-import os
+
+from curses import meta
 from dataclasses import dataclass, field
-from typing import Any, List
+from typing import Any, Dict, List, Optional
 from pathlib import Path
 
+import time
 import hydra
-from omegaconf import DictConfig
+from omegaconf import DictConfig, MISSING
+from mephisto.operations.hydra_config import RunScriptConfig
 from mephisto.operations.hydra_config import register_script_config
-from parlai.crowdsourcing.utils.mturk import MTurkRunScriptConfig
 
 from darma_chat.impl import run_task
 
@@ -28,7 +30,35 @@ defaults = ["_self_", {"conf": "darma"}]
 
 
 @dataclass
-class ScriptConfig(MTurkRunScriptConfig):
+class MTurkConfig:
+    """
+    Add MTurk-specific flags.
+    """
+
+    worker_blocklist_paths: Optional[str] = field(
+        default=MISSING,
+        metadata={
+            "help": (
+                'Path(s) to a list of IDs of workers to soft-block, separated by newlines. Use commas to indicate multiple lists'
+            )
+        },
+    )
+
+    allowed_locales: List[Dict] = field(
+        default=MISSING,
+        metadata={
+            "help": (
+                'List of locales for HITs'
+            )
+        },
+    )
+
+
+
+@dataclass
+class DarmaRunConfig(RunScriptConfig):
+
+    current_time: int = int(time.time())  # For parametrizing block_qualification
     defaults: List[Any] = field(default_factory=lambda: defaults)
     task_dir: str = TASK_DIRECTORY
     monitoring_log_rate: int = field(
@@ -37,9 +67,10 @@ class ScriptConfig(MTurkRunScriptConfig):
             'help': 'Frequency in seconds of logging the monitoring of the crowdsourcing task'
         },
     )
+    mturk: MTurkConfig = MTurkConfig()
 
 
-register_script_config(name='scriptconfig', module=ScriptConfig)
+register_script_config(name='scriptconfig', module=DarmaRunConfig)
 
 
 @hydra.main(config_path="hydra_configs", config_name="scriptconfig")
