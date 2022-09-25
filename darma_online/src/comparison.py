acec6caba11035b597c40c9a1b1887c08642016c
Darpan Jain <darpan.pmun@gmail.com>
2022-09-25 11:18:57 -0700

    Compute comparison scores in splits

--- darma_online/src/comparison.py
+++ darma_online/src/comparison.py
@@ -10,15 +10,20 @@ from darma_online.logging_setup import setup_logger
 
 def get_perspective_scores():
     data_dir = '/isi_darma/isi_darma/darma_online/src/darma_online/data'
+    logger = setup_logger('comparison', 'logs/comparison.log', test=False)
+
     df = read_comments(data_dir)
     df['Model Decision'] = df['model_score'].apply(to_binary)
-    tick = time.time()
-    df['Perspec Decision'], df['Perspec Score'], df['Perspec - Behav Type'] = call_perspective(df['body'])
-    df.to_csv(f'{data_dir}/output_scores_compared.csv')
-    print(f'DONE in {time.time() - tick} seconds!!')
+    splits = range(0, len(df), 1000)
 
-def call_perspective(comments: List[str]):
-    logger = setup_logger('comparison', 'logs/comparison.log', test=False)
+    for idx, split in enumerate(splits):
+        tick = time.time()
+        df_split = df.iloc[split:split+1000]
+        df_split['Perspec Decision'], df_split['Perspec Score'], df_split['Perspec - Behav Type'] = call_perspective(df_split['body'], logger)
+        df.to_csv(f'{data_dir}/output_scores_compared_{idx}.csv')
+        logger.info(f'Split {idx+1} DONE in {time.time() - tick} seconds!!')
+
+def call_perspective(comments: List[str], logger):
     perspective = PerspectiveAPIModerator(logger)
     decision, score, behav_type = np.array([]), np.array([]), np.array([])
     for idx, comm in tqdm(enumerate(comments), desc='Calling Perspective API', total=len(comments), unit='comments'):
