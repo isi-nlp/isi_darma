10afd1567ae5a217c72465ea513ee8a4f5f81d19
Darpan Jain <darpan.pmun@gmail.com>
2023-03-08 15:23:03 -0800

    Remove the archived version of `darma_online`

--- archived/darma_online/test/test_api.py
+++
@@ -1,9 +0,0 @@
-from requests import post
-
-
-# some dummy stuff
-RTG_API = 'http://localhost:6060/translate'
-source = {"source":["Comment allez-vous?", "Bonne journée", "안녕하세요", "지금 기분이 어때?"]}
-response = post(RTG_API, json=source)
-print(response.json())
-
